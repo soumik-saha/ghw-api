fb606f7ace914f367b5ce1e3c5496aad5d4f50dc
Soumik Saha <sahasoumik1573@gmail.com>
2023-04-07 00:02:45 +0530

    Adding Hackathons

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask
+import fcntl
 
 app = Flask(__name__)
+
+hackathons = {
+    "Bitcamp" : {
+        "start_date": "2023-04-07 12:00:00",
+        "end_date": "2023-04-09 12:00:00",
+        "location": "College park, Maryland",
+        "type": "In-person Only"
+    },
+    "Bon Hackétit" : {
+        "start_date": "2023-04-07 12:00:00",
+        "end_date": "2023-04-09 12:00:00",
+        "location": "Everywhere, Online",
+        "type": "Digital Only"
+    }
+}
 
 @app.route("/")
 def hello_ghw():
     return "<p>Hello, API Week Hackers!</p>"
 
+@app.route("/hackathons", methods=['GET','POST'])
+def getHackathons():
+    # if request.method == 'POST':
+
+    # else:
+    return hackathons
+
 if __name__ == "__main__":
-    app.run(debug= True)
+    app.run(debug = True)
